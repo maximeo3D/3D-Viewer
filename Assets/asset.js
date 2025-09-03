952fc9b007d9bd665eb71509e11cba44800c5ebb
maximeo <m.orlandi@hapticmedia.fr>
2025-09-03 17:30:00 +0200

    switch model

--- Assets/asset.js
+++ Assets/asset.js
@@ -6,21 +6,18 @@
 const assetConfiguration = {
   models: [
     {
-      name: "cube-sphere",           // Nom unique du modèle
-      file: "cube-sphere.glb",       // Fichier 3D à charger (GLB/GLTF)
+      name: "arrow",           // Nom unique du modèle
+      file: "arrow.glb",       // Fichier 3D à charger (GLB/GLTF)
       meshes: [
         {
-          name: "Cube",              // Nom du mesh dans le fichier 3D
+          name: "arrow",              // Nom du mesh dans le fichier 3D
           visible: true,             // Visibilité du mesh individuel (true = visible, false = caché)
           materialSlot1: "red",      // Matériau pour le slot 1 (primitive0)
-          materialSlot2: "blue"      // Matériau pour le slot 2 (primitive1)
         },
         {
-          name: "Sphere",            // Nom du mesh dans le fichier 3D
-          visible: false,            // Visibilité du mesh individuel (true = visible, false = caché)
+          name: "link1",            // Nom du mesh dans le fichier 3D
+          visible: true,            // Visibilité du mesh individuel (true = visible, false = caché)
           materialSlot1: "red",      // Matériau pour le slot 1 (primitive0)
-          materialSlot2: "blue",     // Matériau pour le slot 2 (primitive1)
-          materialSlot3: "yellow"    // Matériau pour le slot 3 (primitive2)
         }
       ],
       position: [0, 0, 0],           // Position [X, Y, Z] dans la scène
